7028c3e78678987c4fdef75029cecbc17bcf6e7f
luis <luisesh1x10@gmail.com>
2024-07-19 13:04:01 -0700

    fix

--- app/models/crypto_currency.rb
+++ app/models/crypto_currency.rb
@@ -1,11 +1,16 @@
 class CryptoCurrency < ApplicationRecord
     validates :initialBalance, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :monthlyInterest, presence: true, numericality: { greater_than_or_equal_to: 0 }
-    def calculateEarns
-        nMonths = 12
-        monthlyEarns = initialBalance * (1 + (monthlyInterest / 100));
-        anualEarns = monthlyEarns *  nMonths
+
+    def calculateEarns()
+        n_months = 12
+        anual_earns = initialBalance
+        n_months.times do
+            anual_earns *= (1 + (monthlyInterest / 100.0))
+        end
+        anual_earns - initialBalance
     end
+      
 
     def calculateAssetBalance(price)
         (1 / price.to_f) * initialBalance
